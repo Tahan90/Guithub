9df57738a6b4de47124c5e95a8440840537f8522
David Messing <david.messing@gmail.com>
2013-10-01 21:11:29 -0400

    initial project commit

---
+++ Guithub/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guithub::Application.config.secret_token = '10353befa6d05a9994905e6913b312f3b02add3925898423b64ff29ae749fc6c4c51980e757ab9402ec15bf555ee6ca28d2d9e36538082a16df6fc371df1132b'
